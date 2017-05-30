f78add9060739c259807176b3f8e0d453e9250ab
DerekKen <mcm.icm.ssh2014@hotmail.com>
2017-05-31 05:19:48 +0800

    Log  main_activity.xml的显示效果与预览的不同，还在调试

--- java/cn/studyjams/s2/sj0175/findingnankai/MainActivity.java
+++ java/cn/studyjams/s2/sj0175/findingnankai/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //For Android
     }
 
     public void startBtn_OnClick(View v)
