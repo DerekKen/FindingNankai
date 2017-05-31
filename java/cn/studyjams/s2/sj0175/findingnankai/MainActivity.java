166a6c1326307b26c21d6985de1f72ef2684cb66
DerekKen <mcm.icm.ssh2014@hotmail.com>
2017-05-31 12:14:04 +0800

    LOG 代码清理

--- java/cn/studyjams/s2/sj0175/findingnankai/MainActivity.java
+++ java/cn/studyjams/s2/sj0175/findingnankai/MainActivity.java
@@ -17,27 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 
     public void startBtn_OnClick(View v)
     {
         Toast.makeText(this, R.string.start_button_pressed_toast, Toast.LENGTH_LONG).show();
-//        setContentView(R.layout.activity_wandering);
-
 
         //显式Intent的使用  (√)可以正常在活动之间跳转
         Intent wandering_intent = new Intent(MainActivity.this, WanderingActivity.class);
         startActivity(wandering_intent);
 
-
-        //隐式Intent的使用 (√) 成功
-//        Intent implicit_wandering_intent = new Intent("android.intent.action.WANDERING_ACTION");
-//        implicit_wandering_intent.addCategory("android.intent.category.CAT_WANDERING");
-//        startActivity(implicit_wandering_intent);
+        //The following code logs a SELECT_CONTENT Event when a user clicks on a specific element in your app.
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "ID_StartButton");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "开始按钮");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
 
     public void exitBtn_OnClick(View v)
     {
+        //The following code logs a SELECT_CONTENT Event when a user clicks on a specific element in your app.
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "ID_ExitButton");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "退出按钮");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
         //结束Main Activity，与Back导航按键一样
         finish();
     }
--- java/cn/studyjams/s2/sj0175/findingnankai/WanderingActivity.java
+++ java/cn/studyjams/s2/sj0175/findingnankai/WanderingActivity.java
@@ -12,19 +12,11 @@ public class WanderingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wandering);
-
-//        ImageView imageView = (ImageView) findViewById(R.id.schoolgate_NKU);
-//        Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
-
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     public void schoolGate_OnClick(View v)
     {
-//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        //继续"发现南开"，在校园中游玩：新开湖
-//        setContentView(R.layout.activity_wandering_02);
-
         Intent intent = new Intent(WanderingActivity.this, ScenariesList.class);
         startActivity(intent);
     }
