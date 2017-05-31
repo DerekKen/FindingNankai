a470ac55e2bbb310c3533c2466ab9e1e7b81581d
DerekKen <mcm.icm.ssh2014@hotmail.com>
2017-05-31 09:37:13 +0800

    S.A.P

---
+++ java/cn/studyjams/s2/sj0175/findingnankai/ScenariesList.java
@@ -0,0 +1,13 @@
+package cn.studyjams.s2.sj0175.findingnankai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScenariesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scenaries_list);
+    }
+}
--- java/cn/studyjams/s2/sj0175/findingnankai/WanderingActivity.java
+++ java/cn/studyjams/s2/sj0175/findingnankai/WanderingActivity.java
@@ -1,5 +1,6 @@
 package cn.studyjams.s2.sj0175.findingnankai;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,14 +12,20 @@ public class WanderingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wandering);
+
+//        ImageView imageView = (ImageView) findViewById(R.id.schoolgate_NKU);
+//        Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     public void schoolGate_OnClick(View v)
     {
-        
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         //继续"发现南开"，在校园中游玩：新开湖
-        setContentView(R.layout.activity_wandering_02);
+//        setContentView(R.layout.activity_wandering_02);
+
+        Intent intent = new Intent(WanderingActivity.this, ScenariesList.class);
+        startActivity(intent);
     }
 }
