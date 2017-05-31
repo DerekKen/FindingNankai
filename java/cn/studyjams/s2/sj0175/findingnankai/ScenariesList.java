c1653f2208492b762763a03cbe88e47d402f4a6d
DerekKen <mcm.icm.ssh2014@hotmail.com>
2017-05-31 11:17:28 +0800

    LOG 还差一些资源需要替换；S.A.P

--- java/cn/studyjams/s2/sj0175/findingnankai/ScenariesList.java
+++ java/cn/studyjams/s2/sj0175/findingnankai/ScenariesList.java
@@ -16,9 +16,6 @@ public class ScenariesList extends AppCompatActivity {
 
     public void on_xinkai_lake_btn_click(View v)
     {
-//        Intent intent = new Intent(this, ScenariesList.class);
-//        startActivity(intent);
-
         setContentView(R.layout.activity_xin_kai_lake);
     }
 
@@ -26,4 +23,20 @@ public class ScenariesList extends AppCompatActivity {
     {
         setContentView(R.layout.activity_scenaries_list);
     }
+
+    public void on_memorial_btn_click(View v)
+    {
+        setContentView(R.layout.memorial);
+    }
+
+    public void on_zhulou_button_click(View v)
+    {
+        setContentView(R.layout.zhulou);
+    }
+
+    public void on_matihu_button_click(View v)
+    {
+        setContentView(R.layout.ma_ti_lake);
+    }
+
 }
